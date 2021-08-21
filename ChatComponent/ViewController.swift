b3d59ac58f1cdbcc61b980e97c0b1a019bdd8fab
Nikita <nkonashenko@gmail.com>
2021-08-21 16:22:18 +0300

    Initial Commit

---
+++ ChatComponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatComponent
+//
+//  Created by Nikita Konashenko on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
